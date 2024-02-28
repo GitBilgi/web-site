45a6dcc732310d4a51d52d439602445ce9dda47f
Tainã Pino <tmartins.pino@gmail.com>
2024-02-28 09:17:24 -0300

    chore: adds component TextArea

--- src/components/core/Form/Fields/TextArea/TextArea.stories.tsx
+++ src/components/core/Form/Fields/TextArea/TextArea.stories.tsx
@@ -3,7 +3,11 @@ import { TextArea } from '.'
 
 export default {
   title: 'TextArea',
-  component: TextArea
+  component: TextArea,
+  args: {
+    label: 'Mensagem',
+    placeholder: 'Deixe nos uma mensagem...'
+  }
 } as Meta
 
 export const Default: StoryObj = {}
--- src/components/core/Form/Fields/TextArea/TextArea.tsx
+++ src/components/core/Form/Fields/TextArea/TextArea.tsx
@@ -1,9 +1,93 @@
-import * as S from './TextArea.styles'
+import { useEffect, useRef, useState } from 'react'
 
-export function TextArea() {
+import { v4 as uuidv4 } from 'uuid'
+
+import { ExclamationCircleIcon } from '@heroicons/react/24/outline'
+
+import { HelperText } from '../../HelperText'
+import { Icon } from '../../../Icon'
+
+import * as S from './TextArea.styles'
+import { Label } from '../../Label'
+
+interface Props {
+  label?: string
+  name: string
+  placeholder?: string
+  initialValue?: string
+  value?: string
+  maxlength?: number
+  error?: boolean
+  helperText?: string
+  readOnly?: boolean
+  disabled?: boolean
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
+  onBlur?: (e: React.FocusEvent<HTMLTextAreaElement>) => void
+}
+
+export function TextArea({
+  label,
+  name,
+  placeholder,
+  initialValue,
+  value,
+  maxlength,
+  error,
+  helperText,
+  readOnly,
+  disabled,
+  onChange,
+  onBlur
+}: Props) {
+  const textAreaField = useRef<HTMLTextAreaElement | null>(null)
+
+  const [textAreaValue, setTextAreaValue] = useState(initialValue || '')
+
+  useEffect(() => {
+    typeof value === 'string' && setTextAreaValue(value)
+  }, [value])
+
+  function handleOnChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    onChange && onChange(e)
+
+    setTextAreaValue(e.target.value)
+  }
+
   return (
-    <S.Wrapper>
-      <h1>TextArea</h1>
-    </S.Wrapper>
+    <S.Container
+      hasValue={!!textAreaValue}
+      error={error}
+      readOnly={readOnly}
+      disabled={disabled}
+    >
+      {label && <Label htmlFor={name}>{label}</Label>}
+
+      <div className="text-area">
+        <textarea
+          id={uuidv4()}
+          name={name}
+          value={textAreaValue}
+          placeholder={placeholder}
+          maxLength={maxlength}
+          readOnly={readOnly}
+          disabled={disabled}
+          onChange={handleOnChange}
+          onBlur={onBlur}
+          ref={textAreaField}
+          autoComplete="one-time-code"
+        />
+
+        {error && (
+          <Icon
+            size="sm"
+            IconRender={ExclamationCircleIcon}
+            mode="error"
+            className="icon"
+          />
+        )}
+      </div>
+
+      {helperText && <HelperText text={helperText} />}
+    </S.Container>
   )
 }
