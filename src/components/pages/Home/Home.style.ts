4c1458df5a56062dbc59d32d749ed22a4b7b3879
Bruno Souza <brunodesouza_bso@hotmail.com>
2024-03-04 17:51:17 -0300

    feat: add sections

--- src/components/pages/Home/Home.style.ts
+++ src/components/pages/Home/Home.style.ts
@@ -4,33 +4,106 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 100%;
-  height: 580px;
   display: flex;
+  padding-top: 54px;
+  padding-bottom: 54px;
   align-items: center;
-  justify-content: center;
   flex-direction: column;
+  justify-content: center;
 
-  .subtitle {
-    width: 320px;
-    padding: 4px 12px;
-    border-radius: 16px;
-    color: #80162a;
-    background-color: #fff3f0;
-  }
-
-  .content {
-    gap: 24px;
-    width: 592px;
-    height: 244px;
+  .container {
+    width: 100%;
+    height: 580px;
     display: flex;
-    text-align: center;
-    justify-content: center;
     align-items: center;
     flex-direction: column;
-  }
+    justify-content: center;
+    padding: 42px 80px 72px 80px;
 
-  .buttons {
-    gap: 12px;
+    .subtitle {
+      width: 320px;
+      color: #80162a;
+      padding: 4px 12px;
+      border-radius: 16px;
+      background-color: #fff3f0;
+    }
+
+    .content {
+      gap: 24px;
+      width: 592px;
+      height: 244px;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      flex-direction: column;
+      justify-content: center;
+    }
+
+    .buttons {
+      gap: 12px;
+      display: flex;
+      padding-top: 48px;
+    }
+
+    .integrations {
+      gap: 12px;
+      display: flex;
+      padding-top: 48px;
+      flex-direction: column;
+
+      .apps {
+        gap: 16px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  }
+  .image-background {
+    z-index: -1;
+    width: 100%;
+    height: 100%;
     display: flex;
+    position: absolute;
+    align-items: center;
+    justify-content: center;
+  }
+`
+
+export const SocialSection = styled.div`
+  gap: 32px;
+  width: 100%;
+  height: 296px;
+  display: flex;
+  padding: 96px 80px;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #f9fafb;
+
+  .logos {
+    gap: 35.4px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+`
+export const SectionFeatures = styled.div`
+  width: 100%;
+  display: flex;
+
+  .text {
+    padding-left: 80px;
+  }
+  .subtitle {
+    width: 800px;
+    color: #80162a;
+  }
+  .heading {
+    width: 800px;
+  }
+  .paragraph {
+    width: 100%;
   }
 `
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,31 +1,100 @@
 'use client'
 
+import Image from 'next/image'
 import { Heading } from '@/components/core/Typography/Heading'
 import { Paragraph } from '@/components/core/Typography/Paragraph'
-import * as S from './Home.style'
 import { Button } from '@/components/core/Buttons/Button'
+import * as S from './Home.style'
+
+import Notion from '@/images/notion.png'
+import Stack from '@/images/slack.png'
+import GDrive from '@/images/g-drive.png'
+import Jira from '@/images/jira.png'
+import Background from '@/images/background.png'
+import Layers from '@/images/Layers.png'
+import Sisyphus from '@/images/Sisyphus.png'
+import Circooles from '@/images/Circooles.png'
+import Catalog from '@/images/Catalog.png'
+import Quotient from '@/images/Quotient.png'
+import Hourglass from '@/images/Hourglass.png'
+import { Subtitle } from '@/components/core/Typography/Subtitle'
 
 export function HomePage() {
   return (
-    <S.Container>
-      <div className="content">
-        <Paragraph size="sm" className="subtitle">
-          Conectando você ao que importa
-        </Paragraph>
-        <Heading size="lg">Inteligência para Crescimento Estratégico</Heading>
-        <Paragraph size="md">
-          Descomplicamos a gestão para garantir resultados consistentes e
-          previsíveis em cada aspecto crucial do seu negócio.
-        </Paragraph>
-      </div>
-      <div className="buttons">
-        <Button button_size="xxl" styles="primary">
-          Agendar Chamada
-        </Button>
-        <Button button_size="xxl" styles="secondary">
-          Explorar
-        </Button>
-      </div>
-    </S.Container>
+    <>
+      <S.Container>
+        <div className="image-background">
+          <Image src={Background} alt="Imagem de Fundo HomePage" />
+        </div>
+        <div className="container">
+          <div className="content">
+            <Paragraph size="sm" className="subtitle">
+              Conectando você ao que importa
+            </Paragraph>
+            <Heading size="lg">
+              Inteligência para Crescimento Estratégico
+            </Heading>
+            <Paragraph size="md">
+              Descomplicamos a gestão para garantir resultados consistentes e
+              previsíveis em cada aspecto crucial do seu negócio.
+            </Paragraph>
+          </div>
+          <div className="buttons">
+            <Button button_size="xxl" styles="primary">
+              Agendar Chamada
+            </Button>
+            <Button button_size="xxl" styles="secondary">
+              Explorar
+            </Button>
+          </div>
+          <div className="integrations">
+            <Paragraph size="sm">Integrações Inteligentes</Paragraph>
+            <div className="apps">
+              <Image src={Notion} alt="Logo Notion" />
+              <Image src={Stack} alt="Logo Stack" />
+              <Image src={GDrive} alt="Logo Google Drive" />
+              <Image src={Jira} alt="Logo Jira" />
+            </div>
+          </div>
+        </div>
+      </S.Container>
+      <S.SocialSection>
+        <div className="paragraph">
+          <Paragraph size="sm">
+            Junte-se a mais de X.000 empresas que já estão crescendo
+          </Paragraph>
+        </div>
+        <div className="logos">
+          <Image src={Layers} alt="Logo Layers" />
+          <Image src={Sisyphus} alt="Logo Sisyphus" />
+          <Image src={Circooles} alt="Logo Circooles" />
+          <Image src={Catalog} alt="Logo Catalog" />
+          <Image src={Quotient} alt="Logo Quotient" />
+          <Image src={Hourglass} alt="Loogo Hourglass" />
+        </div>
+      </S.SocialSection>
+      <S.SectionFeatures>
+        <div className="inteligence">
+          <div className="text">
+            <div>
+              <Subtitle size="sm" className="subtitle">
+                Por que a Bilgi Intelligence?
+              </Subtitle>
+            </div>
+            <div className="heading">
+              <Heading size="md">
+                A única consultoria de que você precisa. Simples.
+              </Heading>
+            </div>
+            <div className="paragraph">
+              <Paragraph size="lg">
+                Otimize seus gastos, reduza custos e deixe a parte complicada
+                com a gente.
+              </Paragraph>
+            </div>
+          </div>
+        </div>
+      </S.SectionFeatures>
+    </>
   )
 }
