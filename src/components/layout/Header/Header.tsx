135be53e223eb1cfc60121ee34b0b86a38427f11
tiago <tiago@bilgi.co.br>
2025-04-05 23:42:19 -0300

    feat: tag google

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -15,6 +15,8 @@ import { Icon } from '@/components/core/Icon'
 import { Bars2Icon } from '@heroicons/react/24/solid'
 import { useRouter } from 'next/navigation'
 
+import Head from 'next/head' // Importando Head do Next.js
+
 export function Header() {
   const { isMobile } = useDeviceDetection()
   const [menuIsVisible, setMenuIsVisible] = useState<boolean>(false)
@@ -22,53 +24,72 @@ export function Header() {
   const router = useRouter()
 
   return (
-    <S.Container isMobile={isMobile}>
-      {!isMobile ? (
-        <S.Content>
-          <div className="logo">
-            <Image src={Logo} alt="Logo Bilgi" />
-          </div>
+    <>
+      {/* Adicionando a tag do Google no Head */}
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-LYKMMJ39G8"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LYKMMJ39G8');
+          `}
+        </script>
+      </Head>
 
-          <S.Nav>
-            {NavForm.map((item, idx) => (
-              <ButtonLink key={idx} size="md" link={item.link} mode="gray">
-                {item.children}
-              </ButtonLink>
-            ))}
-          </S.Nav>
+      <S.Container isMobile={isMobile}>
+        {!isMobile ? (
+          <S.Content>
+            <div className="logo">
+              <Image src={Logo} alt="Logo Bilgi" />
+            </div>
 
-          <div className="button">
-            <Button
-              button_size="sm"
-              styles="primary"
-              onClick={() => router.push('/form')}
-            >
-              Agendar Chamada
-            </Button>
-          </div>
-        </S.Content>
-      ) : (
-        <div className="menu-bar">
-          <div className="LogoImage">
-            <Image src={Logo} alt="Logo Bilgi" />
-          </div>
+            <S.Nav>
+              {NavForm.map((item, idx) => (
+                <ButtonLink key={idx} size="md" link={item.link} mode="gray">
+                  {item.children}
+                </ButtonLink>
+              ))}
+            </S.Nav>
 
-          <div className="hamburguer">
-            <div onClick={() => setMenuIsVisible(true)}>
-              <Icon
-                size="lg"
-                className="icon"
-                IconRender={Bars2Icon}
-                mode={'hard-gray'}
-              />
+            <div className="button">
+              <Button
+                button_size="sm"
+                styles="primary"
+                onClick={() => router.push('/form')}
+              >
+                Agendar Chamada
+              </Button>
+            </div>
+          </S.Content>
+        ) : (
+          <div className="menu-bar">
+            <div className="LogoImage">
+              <Image src={Logo} alt="Logo Bilgi" />
+            </div>
+
+            <div className="hamburguer">
+              <div onClick={() => setMenuIsVisible(true)}>
+                <Icon
+                  size="lg"
+                  className="icon"
+                  IconRender={Bars2Icon}
+                  mode={'hard-gray'}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      )}
-      <MenuMobile
-        menuIsVisible={menuIsVisible}
-        setMenuIsVisible={setMenuIsVisible}
-      />
-    </S.Container>
+        )}
+        <MenuMobile
+          menuIsVisible={menuIsVisible}
+          setMenuIsVisible={setMenuIsVisible}
+        />
+      </S.Container>
+    </>
   )
 }
